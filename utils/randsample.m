2aa61c23cea184a2120d1004acce4fe0232b66e7
Jacob Eisenstein <jacobe@gmail.com>
2013-05-03 14:40:41 -0400

    added single argument functionality

--- utils/randsample.m
+++ utils/randsample.m
@@ -2,8 +2,8 @@ function y = randsample(n,k,j)
 %function y = randsample(n,k)
 %
 %sample without replacement k integers from 1:n
-
-if nargin==2, j = 1; end
+if nargin<2, k = n; end
+if nargin<3, j = 1; end
 if numel(n) == 1
     n = 1:n;
 end
