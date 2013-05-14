31e31ea70940355ea107ff07eaf8f9a73e69ae5a
jacobeisenstein <jacobe@gmail.com>
2013-05-14 18:35:37 -0400

    added functionality to sortidxs

--- utils/sortidxs.m
+++ utils/sortidxs.m
@@ -1,4 +1,5 @@
-function [ idx val ] = sortidxs( x, dim, mode, topk )
+function [ idx val ] = sortidxs( x, dim, mode, topk, varargin )
+[lo_thresh, hi_thresh] = process_options(varargin,'lo-thresh',-Inf,'hi-thresh',Inf);
 %function [ idx val ] = sortidxs( x, dim, mode, topk )
 if nargin < 2, 
     dim = 2;
@@ -7,9 +8,11 @@ if nargin < 3
     mode = 'descend';
 end
 [val idx] = sort(x,dim,mode);
-if nargin == 4
+if nargin >= 4
     idx = idx(1:topk);
     val = val(1:topk);
 end
+idx = idx(val < hi_thresh & val > lo_thresh);
+val = val(val < hi_thresh & val > lo_thresh);
 end
 
